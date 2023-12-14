4601a15e8253df9b1923128a319f04dc588b0e90
Ansar Nurzhanov <nansarik@mail.ru>
2023-12-14 10:58:06 +0600

    feat: http-server adding[flusk]

---
+++ http/flusk.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import platform
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    pi_info = {
+        "System": platform.system(),
+        "Release": platform.release(),
+        "Version": platform.version(),
+        "Processor": platform.processor(),
+        "Uptime": subprocess.check_output(["uptime"]).decode("utf-8")
+    }
+    return render_template('index.html', pi_info=pi_info)
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    # Выполняем команду выключения
+    subprocess.call(['sudo', 'shutdown', '-h', 'now'])
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
