f35628ffd64d86010865299c6bd2b15c145bfce0
Ansar Nurzhanov <nansarik@mail.ru>
2023-12-14 14:32:18 +0600

    asdsa

--- src/voice_to_text.py
+++ src/voice_to_text.py
@@ -13,42 +13,28 @@ vosk_recognizer = vosk.KaldiRecognizer(model, 16000)
 def init():
     return True
 
+import os
+import sys
+import speech_recognition as sr
+import vosk
+
 def recognize_audio():
     recognizer = sr.Recognizer()
+
+    model = vosk.Model(model_path)
+    recognizer.recognize_vosk = lambda audio_data, language="en-US", show_all=False: model.decode(audio_data, model.get_sample_frequency())
 
     with sr.Microphone() as source:
         print("Say something:")
         audio_data = recognizer.listen(source)
 
     try:
-        audio_data_bytes = audio_data.frame_data
-        vosk_recognizer.AcceptWaveform(audio_data_bytes)
-        result = vosk_recognizer.Result()
-        result_text = result["text"]
-
-        return result_text
+        result = recognizer.recognize_vosk(audio_data)
+        print("You said:", result)
+        return result
     except sr.UnknownValueError:
         print("Sorry, could not understand audio.")
         return None
     except sr.RequestError as e:
         print(f"Error during Vosk recognition; {e}")
         return None
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        return None
-
-if __name__ == "__main__":
-    if not os.path.exists(model_path):
-        print(f"Vosk model path {model_path} not found.")
-        sys.exit(1)
-
-    while True:
-        command = recognize_audio()
-        if command:
-            if "hello" in command.lower():
-                print("Hello! How can I help you?")
-            elif "goodbye" in command.lower():
-                print("Goodbye!")
-                break
-            else:
-                print("Command not recognized. Try again.")
