97b42d59fd39b765a176e33c3991ceab5ca5c3e4
Ansar Nurzhanov <nansarik@mail.ru>
2023-12-14 14:19:21 +0600

    update

--- src/voice_to_text.py
+++ src/voice_to_text.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import queue
+import threading
+import struct
 import speech_recognition as sr
 import vosk
 from dotenv import load_dotenv
@@ -17,23 +19,23 @@ audio_queue = queue.Queue()
 def init():
     return True
 
-def audio_callback(audio_data, frame_count, time_info, status):
-    # Put audio frames into the queue
-    audio_queue.put(audio_data)
-    return None, 0
-
-def recognize_audio():
+def audio_capture():
     recognizer = sr.Recognizer()
 
     with sr.Microphone() as source:
-        print("Say something:")
-        audio_data = recognizer.listen(source, audio_callback=audio_callback)
+        while True:
+            try:
+                audio_data = recognizer.listen(source)
+                audio_queue.put(audio_data)
+            except Exception as e:
+                print(f"Error capturing audio: {e}")
 
+def recognize_audio():
     try:
-        # Process audio frames from the queue
         audio_data_frames = []
         while not audio_queue.empty():
-            audio_data_frames.append(audio_queue.get())
+            audio_data = audio_queue.get()
+            audio_data_frames.append(audio_data)
 
         audio_data_bytes = struct.pack('<' + 'h' * len(audio_data_frames), *audio_data_frames)
         vosk_recognizer.AcceptWaveform(audio_data_bytes)
@@ -55,6 +57,10 @@ if __name__ == "__main__":
     if not os.path.exists(model_path):
         print(f"Vosk model path {model_path} not found.")
         sys.exit(1)
+
+    # Start the audio capture thread
+    audio_thread = threading.Thread(target=audio_capture, daemon=True)
+    audio_thread.start()
 
     while True:
         command = recognize_audio()
