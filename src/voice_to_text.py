a513c095dded8770133321b3a2b2f7e553594da1
Ansar Nurzhanov <nansarik@mail.ru>
2023-12-14 14:34:28 +0600

    asdas

--- src/voice_to_text.py
+++ src/voice_to_text.py
@@ -1,28 +1,25 @@
 import os
-import sys
 import speech_recognition as sr
-import vosk
+from vosk import Model, KaldiRecognizer
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model_path = os.getenv("MODEL_PATH")
-model = vosk.Model(model_path)
-vosk_recognizer = vosk.KaldiRecognizer(model, 16000)
+model = Model(model_path)
 
-def init():
-    return True
+class VoskRecognizer(sr.Recognizer):
+    def recognize_vosk(self, audio_data, language="en-US", show_all=False):
+        vosk_recognizer = KaldiRecognizer(model, model.get_sample_frequency())
+        result = vosk_recognizer.AcceptWaveform(audio_data.frame_data)
 
-import os
-import sys
-import speech_recognition as sr
-import vosk
+        if show_all:
+            return result
+        else:
+            return result["text"]
 
 def recognize_audio():
-    recognizer = sr.Recognizer()
-
-    model = vosk.Model(model_path)
-    recognizer.recognize_vosk = lambda audio_data, language="en-US", show_all=False: model.decode(audio_data, model.get_sample_frequency())
+    recognizer = VoskRecognizer()
 
     with sr.Microphone() as source:
         print("Say something:")
@@ -30,7 +27,7 @@ def recognize_audio():
 
     try:
         result = recognizer.recognize_vosk(audio_data)
-        print("You said:", result)
+        print("Vosk Result:", result)
         return result
     except sr.UnknownValueError:
         print("Sorry, could not understand audio.")
@@ -38,3 +35,22 @@ def recognize_audio():
     except sr.RequestError as e:
         print(f"Error during Vosk recognition; {e}")
         return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
+
+if __name__ == "__main__":
+    if not os.path.exists(model_path):
+        print(f"Vosk model path {model_path} not found.")
+        sys.exit(1)
+
+    while True:
+        command = recognize_audio()
+        if command:
+            if "hello" in command.lower():
+                print("Hello! How can I help you?")
+            elif "goodbye" in command.lower():
+                print("Goodbye!")
+                break
+            else:
+                print("Command not recognized. Try again.")
