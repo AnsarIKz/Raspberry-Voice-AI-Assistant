7c9f0a2989b9c06ddf1f3a8fdb7f04b530425290
NecronTrooper <97604933+NecronTrooper@users.noreply.github.com>
2023-12-14 11:10:55 +0600

    fix: txt2sp

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -3,7 +3,7 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[2].id)  # You can experiment with different indices
-engine.setProperty('rate', 120)
+engine.setProperty('rate', 130)
 
 text = "Hello, this is a text-to-speech example."
 engine.say(text)
