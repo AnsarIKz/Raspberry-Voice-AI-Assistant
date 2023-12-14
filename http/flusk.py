ae6f6a5dea3be043c2d11a1d5a4faa87aa44ce4f
Ansar Nurzhanov <nansarik@mail.ru>
2023-12-14 13:09:16 +0600

    uoasda

--- http/flusk.py
+++ http/flusk.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import platform
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'template')
 
 @app.route('/')
 def index():
